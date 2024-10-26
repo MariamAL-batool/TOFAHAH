73527fa348736800acf8e56595d3effd53c36856
Mariam AL-batool <mariamalbatoolfarrah@gmail.com>
2024-10-26 19:57:38 +0300

    first

--- Assets/skripts/GitHubTest.cs
+++ Assets/skripts/GitHubTest.cs
@@ -7,12 +7,13 @@ public class GitHubTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("meme");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
